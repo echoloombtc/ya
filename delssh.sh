6487c004f4b783e708c94a5f192a7eddcfd2be0a
llowq <96781765+kenDevXD@users.noreply.github.com>
2023-07-19 09:06:07 +0700

    Create delssh.sh

---
+++ delssh.sh
@@ -0,0 +1,11 @@
+fi
+echo -e "\e[32mloading...\e[0m"
+clear
+read -p "Username SSH to Delete : " Pengguna
+
+if getent passwd $Pengguna > /dev/null 2>&1; then
+        userdel $Pengguna
+        echo -e "User $Pengguna was removed."
+else
+        echo -e "Failure: User $Pengguna Not Exist."
+fi
